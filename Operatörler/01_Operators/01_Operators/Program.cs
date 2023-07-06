256b48886f1d0e4c2396321468e1153901fb7a86
Berk <136861042+berkcavdar@users.noreply.github.com>
2023-07-06 15:24:26 +0300

    Add files via upload

---
+++ Operatörler/01_Operators/01_Operators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+//Assignment 
+
+int x_variable = 3;
+int y_variable = 4;
+
+//Operations with assignments
+
+y_variable = y_variable + 2; // y_variable += 2 alternative method
+x_variable = x_variable * 2; // x_variable *=2 alternative method
+
+Console.WriteLine(y_variable);
+Console.WriteLine(x_variable);
+
+y_variable = y_variable / 2;
+Console.WriteLine(y_variable);
+
+//Logical Operators
+// || , && , !
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+// If both of them are true , output as Perfect!
+if(isSuccess && isCompleted)
+{
+    Console.WriteLine("Perfect!");
+}
+// Either one of them or both of them are true , output as Perfect!
+
+if (isSuccess || isCompleted)
+{
+    Console.WriteLine("Perfect!");
+}
+
+//Output as Fine because of both of them are true bool
+if (isSuccess && !isCompleted)
+{
+    Console.WriteLine("Fine!");
+}
+
+//<,>,<=,>=,==,!= Operators
+
+int a_variable = 3;
+int b_variable = 4;
+bool result = a < 4; // means true
+
