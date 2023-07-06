eed94f667b50a3ad7ac39574a00dfd9dfaa25102
Berk <136861042+berkcavdar@users.noreply.github.com>
2023-07-06 17:45:08 +0300

    Add files via upload

---
+++ If-Else-If/04_If_Else_If/04_If_Else_If/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+int dateTime = DateTime.Now.Hour;
+
+if (dateTime >= 6 && dateTime <11)
+{
+    Console.WriteLine("Good Morning");
+}
+
+
+else if (dateTime <= 18)
+{
+    Console.WriteLine("Have a nice day!");
+}
+
+else
+{
+    Console.WriteLine("Good Night!");
+}
+
+string result = dateTime <= 18 ? "Good Days " : "Good Night ";
+
+Console.WriteLine(result);
+
+result = dateTime >= 6 && dateTime < 11 ? "Good Morning " : dateTime <= 18 ? "Have a nice day!" : "Good Nigth";
+Console.WriteLine(result);
